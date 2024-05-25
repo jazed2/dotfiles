f53e306d3d7d1a6e2efa41810f1e1a55876b34f0
jazed2 <jazze.martine@gmail.com>
2024-05-25 17:49:46 +0530

    Added preview support using 'jstkdng/ueberzugpp'

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,16 +3,18 @@ alias vim="nvim $1"
 alias vimrc="nvim ~/.config/nvim/"
 alias zshrc="nvim ~/.config/zsh-config/" 
 
-# Replace Exa with ls
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons'  # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
-alias l.='exa -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+# Replace Eza with ls
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.='eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 
 # Lazy aliasing
 alias poweroff='sudo shutdown -h now' # -h flags halts system, different from normal arch behavior. Required in artix because impatience
 
 # feh wallpaper swallow
-alias fh="/usr/local/bin/devour feh -g 640x480 -d -S filename $1"
+alias feh="/usr/local/bin/devour feh -g 640x480 -d -S filename $1"
 
+# LF aliases 
+alias lf='$HOME/.config/lf/lfub'
