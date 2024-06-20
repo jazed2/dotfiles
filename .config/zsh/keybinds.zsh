e5b6bda7bbe403314bf0a16c830ee805f83a8968
jazed2 <jazze.martine@gmail.com>
2024-06-20 09:03:29 +0530

    New keybind CTRL + o

--- .config/zsh/keybinds.zsh
+++ .config/zsh/keybinds.zsh
@@ -36,3 +36,13 @@ gam () {
     git commit -m $2
 }
 
+# Use lf to switch directories and bind it to ctrl-o
+lfcd () {
+    tmp="$(mktemp -uq)"
+    $HOME/.config/lf/lfub -last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+    fi
+}
+bindkey -s '^o' '^ulfcd\n'
